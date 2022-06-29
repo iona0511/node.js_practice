ff27950efeae290c0a5b9c9aa2631444ae96c887
iona0511 <lilly800511@gmail.com>
2022-06-29 11:37:05 +0800

    fix page error

--- routes/menu.js
+++ routes/menu.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const db = require(__dirname + '/../modules/mysql-connect');
-
 const {
-        toDateString,
-        toDatetimeString,
-    } = require(__dirname + '/../modules/date-tools');
+    toDateString,
+    toDatetimeString,
+} = require(__dirname + '/../modules/date-tools');
 
 const router = express.Router(); // 建立 router 物件
 
-router.get('/', async (req, res)=>{
+const getListHandler = async (req, res)=>{
     let output = {
         perPage: 10,
         page: 1,
         totalRows: 0,
         totalPages: 0,
+        code: 0,  // 辨識狀態
+        error: '',
         rows: []
     };
     let page = +req.query.page || 1;
-    if(page<1){
-        return res.redirect('?page=1');
+    if(page<1) {
+        output.code = 410;
+        output.error = '頁碼太小';
+        return output;
     }
-
-
 
     const sql01 = "SELECT COUNT(1) totalRows FROM menu";
     const [[{totalRows}]] = await db.query(sql01);
@@ -29,19 +30,38 @@ router.get('/', async (req, res)=>{
     if(totalRows) {
         totalPages = Math.ceil(totalRows/output.perPage);
         if(page>totalPages){
-            return res.redirect(`?page=${totalPages}`);
+            output.totalPages = totalPages;
+            output.code = 420;
+            output.error = '頁碼太大';
+            return output;
         }
 
         const sql02 = `SELECT * FROM menu LIMIT ${(page-1)*output.perPage}, ${output.perPage}`;
         const [r2] = await db.query(sql02);
-        r2.forEach(el=> el.created_at = toDatetimeString(el.created_at) );
-
+        r2.forEach(el=> el.birthday = toDateString(el.birthday) );
         output.rows = r2;
     }
-
+    output.code = 200;
     output = {...output, page, totalRows, totalPages};
 
+    return output;
+};
+
+router.get('/', async (req, res)=>{
+    const output = await getListHandler(req, res);
+    switch(output.code){
+        case 410:
+            return res.redirect(`?page=1`);
+            break;
+        case 420:
+            return res.redirect(`?page=${output.totalPages}`);
+            break;
+    }
     res.render('menu/main', output);
+});
+router.get('/api', async (req, res)=>{
+    const output = await getListHandler(req, res);
+    res.json(output);
 });
 
 module.exports = router;
